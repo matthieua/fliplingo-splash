3151676194cbf9aa73e54991a02a37ac6663b719
Matthieu Aussaguel <matthieu.aussaguel@gmail.com>
2013-03-24 21:40:46 +1100

    Actually use 3.2.13

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -2,11 +2,6 @@
 
 # Your secret key for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
-
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-# You can use `rake secret` to generate a secure secret key.
-
-# Make sure your secret_key_base is kept private
-# if you're sharing your code publicly.
-Fliplingo::Application.config.secret_key_base = 'a204a34fa28a98904dcc3ccc9afc509149e4cd4be2d89b3c606afc6b9098ab22921c1057e96f79894818a94778fab0f40b68cc2af6ad2fdcbdaddc5d189b1d56'
+Fliplingo::Application.config.secret_token = 'd7f63b0ae8d65df848cfeb726d606f39e3403f5cd9f31c3f2a1349604520024d70d64a06e790980386106593cdfa1c3adb0e30f19a08e028c65bc7224d961e62'
